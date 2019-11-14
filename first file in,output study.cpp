a7d3a1a288009cb9745727a10a323046c72b8c65
Youngbo Go <gyb357@naver.com>
2019-11-14 14:13:12 +0900

    Create first file in,output study.cpp

---
+++ first file in,output study.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+// 파일 생성과 문자 단위 입출력
+int main() {
+	FILE *fp = NULL;
+
+	fp = fopen("sample.txt", "w");
+
+	if (fp == NULL) {
+		printf("파일 열기 실패\n");
+	}
+	else {
+		printf("파일 열기 성공\n");
+	}
+
+	fputc('a', fp);
+	fputc('b', fp);
+	fputc('c', fp);
+
+	fclose(fp);
+
+	return 0;
+}
+*/
